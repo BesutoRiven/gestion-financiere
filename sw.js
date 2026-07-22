20d7a6fe37d5b5bbb78de5c025678552a659c7c4
BesutoRiven <besutoriven@outlook.fr>
2026-07-22 14:23:25 +0200

    Version 1.15.1 - Corrections de l’interface et améliorations mineures

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // IMPORTANT : incrémente ce numéro à chaque déploiement (gf-v32 -> gf-v7...).
-const CACHE = "gf-v1.15.0";
+const CACHE = "gf-v1.15.1";
 const FICHIERS = [
   "index.html", "app.html", "profil.html",
   "styles.css", "config.js", "manifest.json",
